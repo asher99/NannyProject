d5cf9295cb3696ccfab18e4dec1c5b425caa4d2d
אשר אלכסנדר <asherb94@gmail.com>
2017-11-01 23:02:23 +0200

    dotnet

--- dotNet5778_02_5356_5344/dotNet5778_02_5356_5344/Game.cs
+++ dotNet5778_02_5356_5344/dotNet5778_02_5356_5344/Game.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace dotNet5778_02_5356_5344
+namespace dotNet5778_02_5344_5356
 {
     class Game
     {
         public CardStock deck = new CardStock();
+        Player p1, p2;
     }
 }
